5b2e286768eda5ce520d641427b23a123b56a434
bhaveshc20 <bhaveshc789@gmail.com>
2018-05-07 20:53:37 -0700

    done with lab 5

--- src/lab5/Main.java
+++ src/lab5/Main.java
@@ -4,8 +4,12 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 
 public class Main {
+
+  public static HashMap<String, Integer> result = new HashMap<String, Integer>();
 
   public static void main(String[] args) {
     try {
@@ -14,9 +18,24 @@ public class Main {
       String readLine = "";
       while ((readLine = b.readLine()) != null) {
         System.out.println(readLine);
+        parser(readLine);
       }
+      System.out.println(result);
     } catch (IOException e) {
       e.printStackTrace();
+    }
+  }
+
+  public static void parser(String s) {
+    StringTokenizer str = new StringTokenizer(s);
+    while(str.hasMoreTokens()) {
+      String word = str.nextToken();
+      if(result.containsKey(word)) {
+        int temp = result.get(word) + 1;
+        result.put(word, temp);
+      } else {
+        result.put(word, 1);
+      }
     }
   }
 }
